548601319b4a66cdba866584f4a1997ed69a8e93
[Enrique Viveros] <encig@icloud.com>
2025-02-09 19:07:48 +0100

    version 1.5.4 fixing navbar responsiveness for mobile devices

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,46 @@
 $(document).ready(function(){
+  // Burger menu functionality
+  const burgerMenu = $('.burger_menu');
+  const burgerIcon = $('.burger_icon');
+  const navbarMenu = $('.navbar_menu');
 
-    $('#menu').click(function(){
-      $(this).toggleClass('fa-times');
-      $('header').toggleClass('toggle');
-    });
-  
-    $(window).on('scroll load',function(){
-  
-      $('#menu').removeClass('fa-times');
-      $('header').removeClass('toggle');
-  
-      if($(window).scrollTop() > 0){
-        $('.top').show();
-      }else{
-        $('.top').hide();
-      }
-  
-    });
-  
-    // smooth scrolling 
-  
-    $('a[href*="#"]').on('click',function(e){
-  
-      e.preventDefault();
-  
-      $('html, body').animate({
-  
-        scrollTop : $($(this).attr('href')).offset().top,
-  
-      },
-        500, 
-        'linear'
-      );
-  
-    });
-  
+  burgerMenu.click(function(){
+      burgerIcon.toggleClass('active');
+      navbarMenu.toggleClass('active');
   });
+
+  // Close menu when clicking outside
+  $(document).click(function(event) {
+      if (!burgerMenu.is(event.target) && !navbarMenu.is(event.target) && 
+          navbarMenu.has(event.target).length === 0 && !burgerMenu.has(event.target).length) {
+          burgerIcon.removeClass('active');
+          navbarMenu.removeClass('active');
+      }
+  });
+
+  // Handle scroll events
+  $(window).on('scroll load', function(){
+      burgerIcon.removeClass('active');
+      navbarMenu.removeClass('active');
+
+      if($(window).scrollTop() > 0){
+          $('.top').show();
+      }else{
+          $('.top').hide();
+      }
+  });
+
+  // Smooth scrolling
+  $('a[href*="#"]').on('click', function(e){
+      e.preventDefault();
+
+      // Close burger menu when clicking a link
+      burgerIcon.removeClass('active');
+      navbarMenu.removeClass('active');
+
+      // Smooth scroll to section
+      $('html, body').animate({
+          scrollTop : $($(this).attr('href')).offset().top,
+      }, 500, 'linear');
+  });
+});
